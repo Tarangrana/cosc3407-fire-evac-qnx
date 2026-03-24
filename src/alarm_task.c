3eadc124335fbeead6fab7c67f3667141631db77
Tarang Rana <tarangrana48@gmail.com>
2026-03-24 15:44:54 -0400

    Alram and exit task added

--- src/alarm_task.c
+++ src/alarm_task.c
@@ -1,1 +1,41 @@
-#include "shared.h" //declare shared variables and function prototypes
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include "shared.h"
+
+// Buzzer GPIO pin (you'll define this based on your hardware setup)
+#define BUZZER_PIN 17  // Example GPIO pin
+
+void* alarm_task(void* arg) {
+    (void)arg;
+    
+    int buzzer_active = 0;
+    
+    printf("[Alarm] Task started (priority: HIGH)\n");
+    
+    while (1) {
+        // Check fire status with semaphore protection
+        sem_wait(&fire_sem);
+        int current_fire = fire_status;
+        sem_post(&fire_sem);
+        
+        // Control buzzer based on fire status
+        if (current_fire && !buzzer_active) {
+            // Activate buzzer
+            printf("[Alarm] FIRE DETECTED! Activating alarm...\n");
+            // TODO: Add GPIO code to activate buzzer
+            buzzer_active = 1;
+        }
+        else if (!current_fire && buzzer_active) {
+            // Deactivate buzzer
+            printf("[Alarm] Fire cleared. Deactivating alarm...\n");
+            // TODO: Add GPIO code to deactivate buzzer
+            buzzer_active = 0;
+        }
+        
+        sleep(1);  // Check every second
+    }
+    
+    return NULL;
+}
--- src/exit_task.c
+++ src/exit_task.c
@@ -1,1 +1,46 @@
-#include "shared.h" //declare shared variables and function prototypes
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include "shared.h"
+
+// LED GPIO pins for exit guidance (you'll define based on your setup)
+#define EXIT_A_LED 27   // Green LED for normal exit
+#define EXIT_B_LED 22   // Red LED for emergency exit
+
+void* exit_task(void* arg) {
+    (void)arg;
+    
+    printf("[Exit ] Task started (priority: LOW)\n");
+    
+    while (1) {
+        // Check fire status and determine exit path
+        sem_wait(&fire_sem);
+        int current_fire = fire_status;
+        int current_exit = exit_path;
+        sem_post(&fire_sem);
+        
+        if (current_fire) {
+            // Fire detected - recommend emergency exit B
+            sem_wait(&fire_sem);
+            exit_path = 1;  // Exit B
+            sem_post(&fire_sem);
+            
+            printf("[Exit ] EMERGENCY: Directing to Exit B\n");
+            // TODO: Add GPIO code to light up Exit B LED
+        }
+        else {
+            // Normal operation - use exit A
+            sem_wait(&fire_sem);
+            exit_path = 0;  // Exit A
+            sem_post(&fire_sem);
+            
+            printf("[Exit ] Normal: Exit A available\n");
+            // TODO: Add GPIO code to light up Exit A LED
+        }
+        
+        sleep(2);  // Update every 2 seconds
+    }
+    
+    return NULL;
+}
